0a3d19c3478cb4752e395ba8d0625ec33c15f304
Balah0311 <167164525+Balah0311@users.noreply.github.com>
2024-05-30 15:42:07 +0530

    Update queue.py

--- IdhayaMusic/plugins/tools/queue.py
+++ IdhayaMusic/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from IdhayaMusic import app
 from IdhayaMusic.misc import db
-from IdhayaMusic.utils import InflexBin, get_channeplayCB, seconds_to_min
+from IdhayaMusic.utils import IdhayaBin, get_channeplayCB, seconds_to_min
 from IdhayaMusic.utils.database import get_cmode, is_active_chat, is_music_playing
 from IdhayaMusic.utils.decorators.language import language, languageCB
 from IdhayaMusic.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await InflexBin(msg)
+        link = await IdhayaBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
