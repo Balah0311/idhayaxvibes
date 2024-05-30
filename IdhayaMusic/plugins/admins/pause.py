d7b8f6f617f97615e40cc65b396b2a86ffbc203d
Balah0311 <167164525+Balah0311@users.noreply.github.com>
2024-05-30 14:11:45 +0530

    Update pause.py

--- IdhayaMusic/plugins/admins/pause.py
+++ IdhayaMusic/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from IdhayaMusic import app
-from IdhayaMusic.core.call import Inflex
+from IdhayaMusic.core.call import Idhaya
 from IdhayaMusic.utils.database import is_music_playing, music_off
 from IdhayaMusic.utils.decorators import AdminRightsCheck
 from IdhayaMusic.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Inflex.pause_stream(chat_id)
+    await Idhaya.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
