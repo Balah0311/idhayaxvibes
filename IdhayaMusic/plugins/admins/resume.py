9616013d3f5f834de07355958373ddbc628fea27
Balah0311 <167164525+Balah0311@users.noreply.github.com>
2024-05-30 14:23:37 +0530

    Update resume.py

--- IdhayaMusic/plugins/admins/resume.py
+++ IdhayaMusic/plugins/admins/resume.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from IdhayaMusic import app
-from IdhayaMusic.core.call import Inflex
+from IdhayaMusic.core.call import Idhaya
 from IdhayaMusic.utils.database import is_music_playing, music_on
 from IdhayaMusic.utils.decorators import AdminRightsCheck
 from IdhayaMusic.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Inflex.resume_stream(chat_id)
+    await Idhaya.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
