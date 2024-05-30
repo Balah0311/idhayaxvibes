edba9287f847e90a4e93aa72b1bc4fd6af5c4ec8
Balah0311 <167164525+Balah0311@users.noreply.github.com>
2024-05-30 12:20:08 +0530

    Create watcher.py

---
+++ IdhayaMusic/plugins/misc/watcher.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from IdhayaMusic import app
+from IdhayaMusic.core.call import Inflex
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Idhaya.stop_stream_force(message.chat.id)
